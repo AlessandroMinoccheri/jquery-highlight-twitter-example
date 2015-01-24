85dbd0e66ed242e484557ffc307bfc2102404577
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2015-01-24 15:40:35 +0100

    update plugin to version 1.0.0

--- js/jquery/plugin/jquery.highlightTwitter.js
+++ js/jquery/plugin/jquery.highlightTwitter.js
@@ -1,21 +1,25 @@
 /*
 Alessandro Minoccheri
-V 0.1.0
-17-05-2014
+V 1.0.0
+20-01-2015
 */
 
 (function ($) {
+	var textSendGlobal = 'START';
+
     $.fn.extend({      
         highlightTwitter: function (argumentOptions) {
             var defaults = {
-				image: "img/twitter.png",
-			}
+				image: 'img/twitter.png',
+			};
+
 			var options = $.extend(defaults, argumentOptions);
 			
 			return this.each(function () {
                 var o = options;
 				var obj = $(this);
 				var textSend = '';
+				var textCut = '';
 
 				getSelectedText = function(){
 		    		if (window.getSelection) {
@@ -23,35 +27,39 @@ V 0.1.0
 			        } else if (document.selection) {
 			            return document.selection.createRange().text;
 			        }
+
 			        return '';
 		    	}
 
 				obj.mouseup(function(e) {
-					if(($(event.target).closest('#img-share-twitter').length) && (textSend != '')){
+					if(($(e.target).closest('#img-share-twitter').length) && (textSend !== '')){
 						var textTotal = textSend + window.location.href;
 						if(textTotal.length > 140){
 							textCut = 140 - window.location.href.length;
 							textSend = textSend.substring(0, textCut);
 						}
 
+						textSendGlobal = textSend;
+
 						window.open(
-						  "http://twitter.com/share?text=" + textSend + "&url=" + window.location.href,
+						  'http://twitter.com/share?text=' + textSend + '&url=' + window.location.href,
 						  '_blank'
 						);
 					}
 					else{
 						var text = getSelectedText();
-						if (text != ''){
+						if (text !== ''){
 							textSend = text;
-							if($(event.target).closest('#img-share-twitter').length){
-								window.location.href = "http://twitter.com/share?text=" + text + "&url=" + window.location.href;
+							textSendGlobal = textSend; 
+							if($(e.target).closest('#img-share-twitter').length){
+								window.location.href = 'http://twitter.com/share?text=' + text + '&url=' + window.location.href;
 							}
 							else{
 								$(document).find('#img-share-twitter').remove();
 
 								var offset = obj.offset();
-								mouseX = Math.min(e.pageX - offset.left - 30);
-								mouseY = Math.min(e.pageY - offset.top - 58);
+								var mouseX = Math.min(e.pageX - offset.left - 30);
+								var mouseY = Math.min(e.pageY - offset.top - 58);
 
 								var img= '<img id="img-share-twitter" src="' + o.image + '" alt="share on twitter" title="Share on Twitter" style="position:absolute; top: ' + mouseY + 'px;  left: ' + mouseX + 'px;">';
 								if(obj.parent().hasClass('wrapped-highlight')){
@@ -69,5 +77,11 @@ V 0.1.0
 				});
             });
         }
-    });     
+    });
+
+	$.extend({
+        getTextSend: function() {
+            return textSendGlobal;
+        }
+    })
 })(jQuery);
